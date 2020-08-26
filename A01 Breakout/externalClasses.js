383fa70c4a26c457f7a59c1aa0320f4f7ccc28cc
unknown <andrewjamesrose@gmail.com>
2020-08-26 23:07:15 +0100

    end of day

--- A01 Breakout/externalClasses.js
+++ A01 Breakout/externalClasses.js
@@ -1,8 +1,12 @@
 class Ball {
     constructor(){
         this.color = color(255);
-        this.radius = 20;
+        this.diameter = 20;
+        this.radius=this.diameter/2;
         this.location = createVector(width/2, height/2);
+        this.velocity = createVector(0, 0);
+        this.inactive = true;
+        const speedBall = 3;
     }
 
     setColor(ballColor){
@@ -11,8 +15,43 @@ class Ball {
 
     draw(){
         fill(this.color);
-        ellipse(this.location.x, this.location.y, this.radius, this.radius);
+        ellipse(this.location.x, this.location.y, this.diameter, this.diameter);
     }
+
+    activate(){
+        this.inactive=false;
+        this.velocity.set(3,-3);
+    }
+
+    update(){
+        this.location.add(this.velocity);
+        //check each edge in turn
+        
+        //top edge
+            if((this.location.y - this.radius) <= 0){ 
+                this.velocity.mult(1,-1)
+                this.update()
+            }
+
+        //right edge
+        if((this.location.x + this.radius) >= width){ 
+            this.velocity.mult(-1,1)
+            this.update()
+        }
+        //left edge
+
+        if((this.location.x - this.radius) <= 0){ 
+            this.velocity.mult(-1 ,1)
+            this.update()
+        }
+        //bottom edge
+        if((this.location.y + this.radius) >= height){ 
+            this.velocity.mult(1,-1)
+            this.update()
+        }
+
+    }
+    
 
 }
 
@@ -23,10 +62,23 @@ class Paddle {
         this.height = 20;
         this.color = color(255);
         this.location = createVector(width/2 - this.width/2, height-35);
+        const speed = 9;
+        this.speed = {
+            right: createVector(speed, 0),
+            left: createVector(-speed, 0)
+        }
     }
 
     setColor(inputColor){
         this.color = inputColor;
+    }
+
+    update(direction){
+        if(direction === 'right'){
+            this.location.add(this.speed.right);
+        } else{
+            this.location.add(this.speed.left);
+        }
     }
 
     draw() {
@@ -38,7 +90,28 @@ class Paddle {
 
 class Block {
 
+    constructor(posX, posY, hits){
+        this.location = createVector(posX, posY);
+        this.width = width/16;
+        this.height = height/20;
+        this.hits = hits;
+        const colorMap = [  color(127,0,0), 
+                            color(0,110,0), 
+                            color(0,0,190)
+                        ];
+        this.color = colorMap[this.hits-1]
+    }
+  
+
+    draw(){
+        fill(this.color);
+        rect(this.location.x, this.location.y, this.width, this.height);       
+    }
     
+    impact(){
+        
+    }
+
 }
 
 
--- A01 Breakout/main.js
+++ A01 Breakout/main.js
@@ -1,20 +1,66 @@
 ///< reference path="./p5.global-mode.d.ts" / >
-
+const backgroundColour = 49;
 
 function setup() {
     // put setup code here
     createCanvas(800, 600);
-    let backgroundColour = 49;
+
     background(backgroundColour);
     gamePaddle = new Paddle();
     gamePaddle.setColor(color('#BB86FC'))
     gameBall = new Ball();
     gameBall.setColor(color('#BB86FC'));
+    testBlock1 = new Block(100, 100, 1);
+    testBlock2 = new Block(100, 200, 2);
+    testBlock3 = new Block(100, 300, 3);
   }     
 
+
+
 function draw(){
-    //ellipse(150, 150, 50, 50);
+    clear();
+    background(backgroundColour);
+    let keyInput = "";
+    keyInput = checkInput();
+    if(keyInput==="right" ||keyInput === "left") {
+        gamePaddle.update(keyInput);
+    }
+
+    if(gameBall.inactive){
+        if (keyInput ==="launch"){
+            gameBall.activate();  
+        }
+    }
+
+    if(!gameBall.inactive){
+        gameBall.update()
+    }
+
+
     gamePaddle.draw();
     gameBall.draw();
+    testBlock1.draw();
+    testBlock2.draw();
+    testBlock3.draw();
+
+    fill(255);
+    text("FrameRate = " + int(getFrameRate()), 700, 20); 
+
+}
+
+function checkInput(){
+    if (keyIsDown(LEFT_ARROW) && !keyIsDown(RIGHT_ARROW)) {
+        return 'left';
+    } 
+
+    if (!keyIsDown(LEFT_ARROW) && keyIsDown(RIGHT_ARROW)) {
+        return 'right';
+    }
+
+    if (keyIsDown(32)) {
+        return 'launch';
+    }
+
+    return "";
 
 }
